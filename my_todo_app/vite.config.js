3b7947691adf3ce9cfe89597d089b3f9c3515a73
nickperi <nicholaspariag27@gmail.com>
2025-09-20 02:31:01 -0400

    make app available offline

--- my_todo_app/vite.config.js
+++ my_todo_app/vite.config.js
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'], // Cache static assets
+          manifest: {
+            name: 'My React PWA',
+            short_name: 'ReactPWA',
+            description: 'A React Vite PWA for offline use',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: 'assets/react.svg',
+                sizes: '192x192',
+                type: 'image/svg',
+              },
+            ],
+          },
+          workbox: {
+            globPatterns: ['**/*.{js,css,html,ico,png,svg,vue,ts}'], // Cache all imports
+          },
+        }),
+  ],
 })
